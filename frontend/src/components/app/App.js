d98e6d7799fe55749820d7e89516f2e1d66d9166
nicoWeb31 <nico.riot@free.fr>
2021-01-17 18:25:42 +0100

    grid systeme

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,7 +1,24 @@
 import "./app.style.scss";
 
 function App() {
-    return <div className="App">App component</div>;
+    return (
+        <div className="App container">
+            <div className="row">
+                <div className="item1 col-3">orange</div>
+                <div className="item2 col-3">vert</div>
+                <div className="item3 col-3">bleu</div>
+                <div className="item4 col-3">jaune</div>
+                <div className="item5 col-6">rouge</div>
+                <div className="item6 col-6">violet</div>
+                <div className="item7">noir</div>
+                <div className="item8">blanc</div>
+                <div className="item9">vert caca</div>
+                <div className="item10">jaune</div>
+                <div className="item11">marron</div>
+                <div className="item12">gris</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
