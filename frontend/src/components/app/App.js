b797a4b932681f0e36d49b07a216a8041b715082
nicoWeb31 <nico.riot@free.fr>
2021-01-17 19:02:10 +0100

    grid systeme with responsive

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -4,14 +4,16 @@ function App() {
     return (
         <div className="App container">
             <div className="row">
-                <div className="cont item1 col-3 col-xs-12 col-medium-6">orange</div>
-                <div className="cont item2 col-3 col-xs-12 col-medium-6">vert</div>
-                <div className="cont item3 col-3 col-xs-12 col-medium-6">bleu</div>
+                <div className="cont item1 col-4 col-xs-12 col-medium-6">orange</div>
+                <div className="cont item2 col-4 col-xs-12 col-medium-6">vert</div>
+                <div className="cont item3 col-4 col-xs-12 col-medium-6">bleu</div>
                 <div className="cont item4 col-3 col-xs-12 col-medium-6">jaune</div>
                 <div className="cont item5 col-6">rouge</div>
                 <div className="cont item6 col-6">violet</div>
-                <div className="cont item7">noir</div>
-                <div className="cont item8">blanc</div>
+                <div className="cont item7 col-12">noir</div>
+
+                <div className="cont item8 col-offset-1 col-3">blanc</div>
+                
                 <div className="cont item9">vert caca</div>
                 <div className="cont item10">jaune</div>
                 <div className="cont item11">marron</div>
