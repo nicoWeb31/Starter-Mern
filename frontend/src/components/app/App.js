533ad9deaa81e2e67db2360f85d75f05b5b4815e
nicoWeb31 <nico.riot@free.fr>
2021-01-17 18:50:01 +0100

    grid systeme

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -4,18 +4,18 @@ function App() {
     return (
         <div className="App container">
             <div className="row">
-                <div className="item1 col-3">orange</div>
-                <div className="item2 col-3">vert</div>
-                <div className="item3 col-3">bleu</div>
-                <div className="item4 col-3">jaune</div>
-                <div className="item5 col-6">rouge</div>
-                <div className="item6 col-6">violet</div>
-                <div className="item7">noir</div>
-                <div className="item8">blanc</div>
-                <div className="item9">vert caca</div>
-                <div className="item10">jaune</div>
-                <div className="item11">marron</div>
-                <div className="item12">gris</div>
+                <div className="cont item1 col-3 col-xs-12 col-medium-6">orange</div>
+                <div className="cont item2 col-3 col-xs-12 col-medium-6">vert</div>
+                <div className="cont item3 col-3 col-xs-12 col-medium-6">bleu</div>
+                <div className="cont item4 col-3 col-xs-12 col-medium-6">jaune</div>
+                <div className="cont item5 col-6">rouge</div>
+                <div className="cont item6 col-6">violet</div>
+                <div className="cont item7">noir</div>
+                <div className="cont item8">blanc</div>
+                <div className="cont item9">vert caca</div>
+                <div className="cont item10">jaune</div>
+                <div className="cont item11">marron</div>
+                <div className="cont item12">gris</div>
             </div>
         </div>
     );
